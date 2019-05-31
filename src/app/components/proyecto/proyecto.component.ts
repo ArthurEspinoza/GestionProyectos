4b4fd30649f1b2130ec7f110095ab8d436fe3079
ArthurEspinoza <arturoeq64@gmail.com>
2019-05-31 11:46:45 -0500

    Se añadio el botón de salir junto con los iconos correspondientes

--- src/app/components/proyecto/proyecto.component.ts
+++ src/app/components/proyecto/proyecto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProyectoService, Proyecto } from '../../services/proyecto.service';
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-proyecto',
   templateUrl: './proyecto.component.html',
@@ -7,8 +8,10 @@ import { ProyectoService, Proyecto } from '../../services/proyecto.service';
 })
 export class ProyectoComponent implements OnInit {
   listaProyectos: Proyecto[] = [];
-  rutaImg: string = "../../../assets/img/add.svg";
-  constructor( private proyectoService: ProyectoService ) { }
+  rutaAdd: string = "../../../assets/img/add.svg";
+  rutaExit: string = "../../../assets/img/salir.svg";
+  constructor( private proyectoService: ProyectoService,
+               private _route: Router ) { }
 
   ngOnInit() {
     this.listaProyectos = this.proyectoService.getProyectos();
@@ -22,5 +25,11 @@ export class ProyectoComponent implements OnInit {
     } else {
       return '#AA4F34';
     }
+  }
+  verProyecto(index){
+    this._route.navigate(['/proyecto', index]);
+  }
+  regresarASesion(){
+    this._route.navigate(['/sesion']);
   }
 }
